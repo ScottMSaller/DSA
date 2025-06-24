2ff2a996c095090d4dca0e5119a5c34bf2fe160a
scottmsaller <scottmsaller@gmail.com>
2025-06-23 20:17:54 -0400

    implemented insert for BST

--- data_structures/binarySearchTree.js
+++ data_structures/binarySearchTree.js
@@ -11,10 +11,10 @@ class BinarySearchTree {
         this.root = null;
     }
 
+
     insert(val){
         //create a new node, so we can insert it into the tree
         const newNode = new Node(val);
-
         //in the case that there is no root node
         if(!this.root){
             this.root = newNode;
@@ -24,8 +24,24 @@ class BinarySearchTree {
             //start traversing the tree at root
             let current = this.root;
             while(true){
-                if(val >current.val )
+                if(current.val === val) return undefined;
+                if(val > current.val ){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    }
+                    else current = current.right;
+                }
+                if(val < current.val){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    }
+                    else current = current.left;
+                }
             }
         }
     }
+
+
 }
